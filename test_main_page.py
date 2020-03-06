c61b2341136a857204044362f1c6db9f52b47802
Aleksandr Vetrov <vetrov.cnc@gmail.com>
2020-03-06 22:33:28 +0200

    Page Elements in Page Object Pattern.

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,19 @@
 from pages.main_page import MainPage
+from pages.locators import MainPageLocators
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = MainPageLocators.LINK
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
     # открываем страницу
     page.open()                      
     # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = MainPageLocators.LINK
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
