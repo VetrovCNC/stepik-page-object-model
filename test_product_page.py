c04fec6995a07cccccb16a0f81654d9f80116c96
Aleksandr Vetrov <vetrov.cnc@gmail.com>
2020-03-07 18:19:47 +0200

    Add test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page. Refactoring BasePage class.

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,6 @@ import pytest
 from pages.product_page import ProductPage
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -53,3 +52,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.add_product_to_basket()
     # Проверяем, что нет сообщения об успехе с помощью is_disappeared
     page.should_be_disappeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
