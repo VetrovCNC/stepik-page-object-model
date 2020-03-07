18a2626c3f8ffbdcee2b8377f3a06de7e4cb8194
Aleksandr Vetrov <vetrov.cnc@gmail.com>
2020-03-07 15:40:00 +0200

    Implementation testing product page.

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main>.price_color')
+    PRODUCT_TITLE = (By.CSS_SELECTOR, 'div.product_main>h1')
+    MESSAGE_PRODUCT_TITLE = (By.CSS_SELECTOR, '#messages .alertinner>strong')
+    MESSAGE_COST_BASKET = (By.CSS_SELECTOR, '#messages .alertinner>p>strong')
---
+++ pages/product_page.py
@@ -0,0 +1,40 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        btn = self.browser.find_element(
+            *ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        btn.click()
+
+    def get_product_title(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.PRODUCT_TITLE)
+        return element.text
+
+    def get_product_price(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.PRODUCT_PRICE)
+        return element.text
+
+    def get_message_product_title(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.MESSAGE_PRODUCT_TITLE)
+        return element.text
+
+    def get_message_cost_basket(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.MESSAGE_COST_BASKET)
+        return element.text
+
+    def check_message_product_has_been_added_to_basket(self):
+        title = self.get_product_title()
+        title_from_message = self.get_message_product_title()
+        assert title == title_from_message, f'Expected "{title}", but "{title_from_message}" was received'
+
+    def check_message_cost_of_the_basket(self):
+        price = self.get_product_price()
+        cost_basket_from_message = self.get_message_cost_basket()
+        assert price == cost_basket_from_message, f'Expected "{price}", but "{cost_basket_from_message}" was received'
